f5b4385704c6a73422a8a28910faf85f19b580d4
Ivan <Ivanv257@gmail.com>
2018-03-04 08:35:14 +0200

    Script - add,sub, multiple and divide functions added

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// add, subtract, multiply and divide functions for Calculator
+add = (num) => {
+  return num + num;
+}
+
+subtract = (num) => {
+  return num - num;
+}
+
+multiply = (num) => {
+  return num * num;
+}
+
+divide = (num) => {
+  return num / num;
+}
