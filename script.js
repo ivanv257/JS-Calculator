c61bbc5b94d2bfc106386c76031ef1ffe953bc65
Ivan <Ivanv257@gmail.com>
2018-03-05 19:46:09 +0200

    Modified all files

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ multiply = (numA, numB) => {
 divide = (numA, numB) => {
   return numA / numB;
 }
+
+let displayScreen = document.querySelector('#resultScreen');
+let button7 = document.querySelector('#num7');
+
+
+button7.addEventListener('click', () => {
+  displayScreen.textContent = 7;
+})
