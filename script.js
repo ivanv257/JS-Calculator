4bec21c8fb0045a4b27a6ee9d2eb0c351b1384f2
ivanv257 <ivanv257@gmail.com>
2018-03-10 11:18:51 -0500

    Delete functions

--- script.js
+++ script.js
@@ -16,22 +16,3 @@ divide = (numA, numB) => {
   return numA / numB;
 }
 
-let displayScreen = document.querySelector('#resultScreen');
-let button7 = document.querySelector('#num7');
-let button8 = document.querySelector('#num8');
-let button9 = document.querySelector('#num9');
-let button4 = document.querySelector('#num4');
-let button5 = document.querySelector('#num5');
-let button6 = document.querySelector('#num6');
-let button1 = document.querySelector('#num1');
-let button1 = document.querySelector('#num2');
-let button1 = document.querySelector('#num3');
-let button1 = document.querySelector('#num0');
-
-button7.addEventListener('click', () => {
-  displayScreen.textContent = 7;
-})
-
-button8.addEventListener('click', () => {
-  displayScreen.textContent = 8;
-})
