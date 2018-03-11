477efa97db9ca321efb2ac1129926ed1444888fb
ivanv257 <ivanv257@gmail.com>
2018-03-11 07:38:51 -0400

    Script debug

--- script.js
+++ script.js
@@ -22,6 +22,6 @@ const buttons = document.querySelectorAll('.unit');
 
 buttons.forEach((button) => {
   button.addEventListener('click', (e) => {
-    resultScreen.innerHTML = button.id;
+    resultScreen.innerHTML = button;
   })
 })
